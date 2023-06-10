4033fc343f30d8ed13692d5e79035d13a53aea63
Reales Myles <rclarence123@gmail.com>
2023-06-10 15:31:57 -0500

    Se configura conexión a la base de datos

--- db/database.go
+++ db/database.go
@@ -5,7 +5,20 @@ import (
 	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
+
+var dsn = "root:123456@tcp(localhost:3306)/goweb?charset=utf8mb4&parseTime=True&loc=Local"
+var Database = func() (db *gorm.DB) {
+	if db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{}); err != nil {
+		fmt.Println("Error en la conexión", err)
+		panic(err)
+	} else {
+		fmt.Println("Conexion exitosa")
+		return db
+	}
+}()
 
 //username: password@tcp(localhost:3306)/database
 
